8ec54076253ceeb17656e75af2d9fdd04a94ff10
Wyfy0107 <nguyenduy010798@gmail.com>
2020-06-29 16:10:54 +0300

    Updates

---
+++ precache-manifest.90e94a678924373e83c77b6aadfbef04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ef20fe996d24446f33b0262cbe6b61",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "dd7a93511d16654e4aa0",
+    "url": "/Calculator/static/css/main.47ff8057.chunk.css"
+  },
+  {
+    "revision": "281f41007749e5861071",
+    "url": "/Calculator/static/js/2.a10090e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.a10090e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd7a93511d16654e4aa0",
+    "url": "/Calculator/static/js/main.ff57b68e.chunk.js"
+  },
+  {
+    "revision": "c6d7ed7bec03af49b8c8",
+    "url": "/Calculator/static/js/runtime-main.64c4e392.js"
+  }
+]);
